ccf2ce154eb5df330c74c6ddea77bf1efc304187
Thiti1012 <s6619410031@sau.ac.th>
2026-01-14 12:30:51 +0700

    commit 01:ทำการสร้างหน้าHome

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_iot_second_app/views/home_ui.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(
+      //เรียกใช้งานคลาสที่เรียกใช้ Widget หลักของแอป (MaterialApp)
+      FlutterIoTSecondApp());
+}
+
+//------------------------------------------------------
+
+class FlutterIoTSecondApp extends StatefulWidget {
+  const FlutterIoTSecondApp({super.key});
+
+  @override
+  State<FlutterIoTSecondApp> createState() => _FlutterIoTSecondAppState();
+}
+
+class _FlutterIoTSecondAppState extends State<FlutterIoTSecondApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //ซ่อนป้าย debug ที่คาดสีแดง
+      debugShowCheckedModeBanner: false,
+      //กำหนดหน้าจอแรก
+      home: HomeUi(),
+      //กำหนด Theme โดยเฉพ่าะ Font หลักของแอป
+      theme: ThemeData(
+          textTheme: GoogleFonts.kanitTextTheme(Theme.of(context).textTheme)),
+    );
+  }
+}
---
+++ lib/views/home_ui.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeUi extends StatelessWidget {
+  const HomeUi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 250.0,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(50.0),
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: 250.0,
+                height: 250.0,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 100.0,
+            ),
+            Text(
+              'สวัสดีชาวโลก IoTSAU',
+              style: TextStyle(
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+            ),
+            Text(
+              'Sounteast Asia University',
+              style: GoogleFonts.itim(
+                fontSize: 22.0,
+                fontWeight: FontWeight.w100,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+            ),
+            Text(
+              'Created by Thiiwpong IoT-SAU 2026',
+              style: GoogleFonts.itim(
+                fontSize: 22.0,
+                fontWeight: FontWeight.w100,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Text('LOGIN'),
+                  style: OutlinedButton.styleFrom(
+                    fixedSize: Size(120.0, 40.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'SIGN UP',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(120.0, 40.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    backgroundColor: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
